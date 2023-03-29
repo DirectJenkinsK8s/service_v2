e91b99b132fbfcf39e6946b26e65849b48a14466
TomRozekrans <TomRozekrans@users.noreply.github.com>
2023-03-29 12:12:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="DirectJenkinsK8s"
+original_name="service_v2"
+original_urlname="service_v2"
+original_description="Awesome service_v2 created by DirectJenkinsK8s"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv service_v2 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
